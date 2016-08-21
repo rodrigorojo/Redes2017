fa39fd876637bb3db72aebde491d1e76c1b67f22
JuanCarlos <juancarloslopez0607@gmail.com>
2016-08-21 14:10:35 -0500

    vista chat

--- Practica2/GUI/LoginWindow.py
+++ Practica2/GUI/LoginWindow.py
@@ -1,26 +1,22 @@
 from PyQt4 import QtGui
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-import os
 from Channel.Channel import Channel
-#from Constants.Constants import *
+from Constants.Constants import *
 
 class Login(QtGui.QDialog):
     def __init__(self, parent=None):
         super(Login, self).__init__(parent)
 
         self.P1 = QLabel(self)
-        #self.P1.setText(Constants().INFOPORT1)
-        self.P1.setText("Cual es mi puerto?:")
+        self.P1.setText(Constants().INFOPORT1)
         self.textP1 = QLineEdit(self)
 
         self.P2 = QLabel(self)
-        #self.P2.setText(Constants().INFOPORT2)
-        self.P2.setText("Cual es el puerto del contacto?:")
+        self.P2.setText(Constants().INFOPORT2)
         self.textP2 = QLineEdit(self)
 
-        #self.buttonLogin = QPushButton(Constants().ACC, self)
-        self.buttonLogin = QPushButton("Acceder", self)
+        self.buttonLogin = QPushButton(Constants().ACC, self)
         self.buttonLogin.clicked.connect(self.login)
 
         layout = QVBoxLayout(self)
@@ -30,11 +26,9 @@ class Login(QtGui.QDialog):
         layout.addWidget(self.textP2)
         layout.addWidget(self.buttonLogin)
 
-        #self.setWindowTitle(Constants().INFO)
-        self.setWindowTitle("Informacion")
+        self.setWindowTitle(Constants().INFO)
 
     def login(self):
         print type(int(self.textP1.text()))
         c = Channel("",int(self.textP1.text()),int(self.textP1.text()))
-
-#if __name__ == '__main__':
+        self.accept()
